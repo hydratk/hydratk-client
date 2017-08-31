16a45b055781a8b4b17e687f567df25977e0ebb3
Petr Rašek <bowman@hydratk.org>
2017-08-31 13:37:39 +0200

    Install distribution

---
+++ build/lib/hydratk/client.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Main HydraTK client module
+
+.. module:: client
+   :platform: Windows
+   :synopsis: Main HydraTK client module
+.. moduleauthor:: Petr Rašek <bowman@hydratk.org>
+
+"""
+
+from hydratk.gui import Gui
+
+def main():
+    """Main method
+    
+    Args:
+        none
+
+    Returns:
+        void
+
+    """
+
+    htk = Gui.get_instance()
+    htk.mainloop()
+
+if __name__ == '__main__':
+    main()
